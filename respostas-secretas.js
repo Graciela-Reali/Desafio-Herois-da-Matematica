6af6061933eb783139d5c3a078288bbd8afbb152
Graciéla Reali <prof.grareali@gmail.com>
2025-09-30 07:39:30 -0300

    Create respostas-secretas.js
    terceiro arquivo

---
+++ respostas-secretas.js
@@ -0,0 +1,14 @@
+// Olá, herói! Aqui é o Computador falando.
+// Eu também sigo a ordem da Liga da Justiça da Matemática! Veja só:
+
+// Missão 1: A Bomba de Confete
+// Eu vejo uma adição (+) e uma multiplicação (*). O General (*) é mais forte!
+// Então, primeiro eu faço 4 * 0 = 0. Depois, 5 + 0 = 5.
+let resultadoM1 = 5 + 4 * 0;
+console.log("O resultado da Missão 1 é:", resultadoM1); // Eu vou mostrar 5!
+
+// Missão 2: O Desafio do Super-Cérebro
+// Opa! Agora eu vejo o Chefão (Parênteses)! Ele manda em tudo!
+// Primeiro eu resolvo o que está dentro dele: 5 + 4 = 9. Depois, 9 * 0 = 0.
+let resultadoM2 = (5 + 4) * 0;
+console.log("O resultado da Missão 2 é:", resultadoM2); // Eu vou mostrar 0!
